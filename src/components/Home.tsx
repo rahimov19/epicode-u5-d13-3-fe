690d12b06a9ee4739bb74362a6e5211b734d488f
Riccardo Gulin <riccardo@nucleode.com>
2023-02-15 11:02:18 +0100

    Adds chat "login" & display users list

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,6 +11,13 @@ import { Message, User } from "../types"
 import { io } from 'socket.io-client'
 
 // 1. When we jump into this page, the socket.io client needs to connect to the server
+// 2. If the connection happens successfully, the server will emit an event called "welcome"
+// 3. If we want to do something when that event happens we shall LISTEN to that event by using socket.on("welcome")
+// 4. Once we are connected we want to submit the username to the server --> we shall EMIT an event called "setUsername" (containing the username itself as payload)
+// 5. The server is listening for the "setUsername" event, when that event is fired the server will broadcast that username to whoever is listening for an event called "loggedIn"
+// 6. If a client wants to display the list of online users, it should listen for the "loggedIn" event
+// 7. In this way the list of online users is updated only during login, but what happens if a new user joins? In this case we are not updating the list
+// 8. When a new user joins server emits another event called "updateOnlineUsersList", this is supposed to update the list when somebody joins or leaves. Clients they should listen for the "updateOnlineUsersList" event to update the list when somebody joins or leaves
 
 const socket = io("http://localhost:3001", { transports: ["websocket"] })
 // if you don't specify the transport ("websocket") socket.io will try to connect to the server by using Polling (old technique)
@@ -19,6 +26,29 @@ const Home = () => {
   const [username, setUsername] = useState("")
   const [message, setMessage] = useState("")
   const [loggedIn, setLoggedIn] = useState(false)
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    socket.on("welcome", welcomeMessage => {
+      console.log(welcomeMessage)
+
+      socket.on("loggedIn", onlineUsersList => {
+        console.log("logged in event:", onlineUsersList)
+        setLoggedIn(true)
+        setOnlineUsers(onlineUsersList)
+      })
+
+      socket.on("updateOnlineUsersList", onlineUsersList => {
+        console.log("A new user connected/disconnected")
+        setOnlineUsers(onlineUsersList)
+      })
+    })
+  })
+
+  const submitUsername = () => {
+    // here we will be emitting a "setUsername" event (the server is already listening for that)
+    socket.emit("setUsername", { username })
+  }
 
   return (
     <Container fluid>
@@ -29,6 +59,7 @@ const Home = () => {
           <Form
             onSubmit={e => {
               e.preventDefault()
+              submitUsername()
             }}
           >
             <FormControl
@@ -60,7 +91,10 @@ const Home = () => {
         <Col md={3}>
           {/* ONLINE USERS SECTION */}
           <div className="mb-3">Connected users:</div>
-
+          {onlineUsers.length === 0 && <ListGroup.Item>Log in to check who is online!!</ListGroup.Item>}
+          <ListGroup>
+            {onlineUsers.map(user => (<ListGroup.Item key={user.socketId}>{user.username}</ListGroup.Item>))}
+          </ListGroup>
         </Col>
       </Row>
     </Container>
